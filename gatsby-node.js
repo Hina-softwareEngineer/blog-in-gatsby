4d915e75bb5b2aba1168a623069ff9891871f295
HINA KHADIM <hinakhadim2002@gmail.com>
2020-09-30 22:12:24 +0500

    client side rendering of pages

---
+++ gatsby-node.js
@@ -0,0 +1,13 @@
+exports.createPages = async function ({ actions }) { 
+    actions.createPage({
+    
+            path: "my-dynamic-page",
+            component: require.resolve(`./src/templates/dynamic-page.tsx`),
+            context: { 
+                // Data passed to context is available
+                // in pageContext props of the template component
+                name: "Zia",
+             },
+        });
+        console.log("End of Gatsby Node File");
+}
